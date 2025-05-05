0dd1905acc4688ab8cdf0af861c71d892408e40c
Noel Quintos <npquintos@gmail.com>
2025-05-05 08:55:54 -0600

    Update astrocore.lua

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
@@ -7,6 +7,13 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 ---@type LazySpec
 return {
+  {
+  "vhyrro/luarocks.nvim",
+  priority = 1000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
+  config = true,
+  },
+  -- { "hrsh7th/nvim-cmp", enabled = false },
+  -- { "lukas-reineke/indent-blankline.nvim", enabled = false },
   "AstroNvim/astrocore",
   ---@type AstroCoreOpts
   opts = {
@@ -18,6 +25,7 @@ return {
       diagnostics = { virtual_text = true, virtual_lines = false }, -- diagnostic settings on startup
       highlighturl = true, -- highlight URLs at start
       notifications = true, -- enable notifications at start
+      guifont = "CodeNewRoman Nerd Font:h16",
     },
     -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
     diagnostics = {
@@ -40,11 +48,16 @@ return {
     -- vim options can be configured here
     options = {
       opt = { -- vim.opt.<key>
-        relativenumber = true, -- sets vim.opt.relativenumber
+        relativenumber = false, -- sets vim.opt.relativenumber
         number = true, -- sets vim.opt.number
         spell = false, -- sets vim.opt.spell
-        signcolumn = "yes", -- sets vim.opt.signcolumn to yes
-        wrap = false, -- sets vim.opt.wrap
+        signcolumn = "auto", -- sets vim.opt.signcolumn to yes
+        wrap = true, -- sets vim.opt.wrap
+        shiftwidth = 4,
+        expandtab = true,
+        hlsearch = true,
+        tabstop = 4,
+        linebreak = true,
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
@@ -60,25 +73,54 @@ return {
         -- second key is the lefthand side of the map
 
         -- navigate buffer tabs
-        ["]b"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
-        ["[b"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
+        ["<C-S-l>"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
+        ["<C-S-h>"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
 
         -- mappings seen under group name "Buffer"
-        ["<Leader>bd"] = {
-          function()
-            require("astroui.status.heirline").buffer_picker(
-              function(bufnr) require("astrocore.buffer").close(bufnr) end
-            )
-          end,
-          desc = "Close buffer from tabline",
-        },
+        -- ["<Leader>bd"] = {
+          -- function()
+          --   require("astroui.status.heirline").buffer_picker(
+          --     function(bufnr) require("astrocore.buffer").close(bufnr) end
+          --   )
+          -- end,
+          -- desc = "Close buffer from tabline",
+        -- },
 
         -- tables with just a `desc` key will be registered with which-key if it's installed
         -- this is useful for naming menus
-        -- ["<Leader>b"] = { desc = "Buffers" },
+        ["<Leader>b"] = { desc = "Buffers" },
 
         -- setting a mapping to false will disable it
-        -- ["<C-S>"] = false,
+        ["<C-S>"] = false,
+                ["<right>"] = {
+          ":let linenum=getpos('.')[1]|:wincmd l|:call cursor(linenum,0)<cr>",
+          desc = "go to same line number at right pane",
+        },
+        ["<left>"] = {
+          ":let linenum=getpos('.')[1]|:wincmd h|:call cursor(linenum,0)<cr>",
+          desc = "go to same line number at left pane",
+        },
+        ["<up>"] = {
+          ":let linenum=getpos('.')[1]|:wincmd k|:call cursor(linenum,0)<cr>",
+          desc = "go to same line number at upper pane",
+        },
+        ["<down>"] = {
+          ":let linenum=getpos('.')[1]|:wincmd j|:call cursor(linenum,0)<cr>",
+          desc = "go to same line number at lower pane",
+        },
+        ["o"] = { "%", desc = "jump to opening/closing pair" },
+        ["T"] = { "zt", desc = "move current line to top" },
+        ["E"] = { "$", desc = "move cursor to end of line" },
+        ["B"] = { "^", desc = "move cursor to beginning of line" },
+        ["P"] = { "]pcf=def<esc>A:<cr>", desc = "convert to function def" },
+        [">"] = { ">gv", desc = "retain selection after indentation" },
+        ["<"] = { "<gv", desc = "retain selection after un-indentation" },
+        ["gf"] = { "?def <cr>w", desc = "go to start of function" },
+        ["gb"] = { "?def <cr>w*", desc = "go to calling function" },
+        ["<A-b>"] = { "gt", desc = "go to next buffer" },
+        ["<A-t>"] = { ":tabn<CR>", desc = "go to next tab" },
+        ["<Leader>Z"] = { ":tab split<CR>", desc = "zoom into current pane" },
+        ["<Leader>z"] = { "mAZZ'A<ESC>T", desc = "un-zoom" },
       },
     },
   },
