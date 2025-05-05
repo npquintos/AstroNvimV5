670a6279b6bd155df85e20d9021737faab47f24c
Noel Quintos <npquintos@gmail.com>
2025-05-05 08:24:03 -0600

    Update community.lua

--- lua/community.lua
+++ lua/community.lua
@@ -7,6 +7,14 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
+  -- { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
+  { import = "astrocommunity.motion.flash-nvim" },
+  { import = "astrocommunity.file-explorer.oil-nvim" },
+  { import = "astrocommunity.colorscheme.bamboo-nvim" },
+  { import = "astrocommunity.note-taking.neorg" },
+  -- { import = "astrocommunity.snippet.nvim-snippets" },
+  { import = "astrocommunity.completion.coq_nvim" },
+  -- { import = "astrocommunity.editing-support.refactoring-nvim" },
+  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },  
 }
