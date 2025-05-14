4f83f210fc85b7f8f9217f6d3f51e58ab235ed26
Noel Quintos <npquintos@gmail.com>
2025-05-14 13:44:12 -0600

    Update astrocore.lua

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -25,7 +25,7 @@ return {
       diagnostics = { virtual_text = true, virtual_lines = false }, -- diagnostic settings on startup
       highlighturl = true, -- highlight URLs at start
       notifications = true, -- enable notifications at start
-      guifont = "CodeNewRoman Nerd Font:h16",
+      guifont = "ComicShannsMonoNerdFont:h16",
     },
     -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
     diagnostics = {
