90221e74b3958189e54fb28fa0f10a6fae102ac7
Noel Quintos <npquintos@gmail.com>
2025-05-19 15:27:08 -0600

    Update user.lua

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -6,35 +6,41 @@
 
 ---@type LazySpec
 return {
-    {
-      "nvim-neorg/neorg",
-      build = ":Neorg sync-parsers",
-      ft = "norg",
-      LocalLeader = ";",
-      dependencies = { "nvim-lua/plenary.nvim" },
-      event = "VeryLazy",
-      opts = {
-        load = {
-          ["core.defaults"] = {
-          }, -- Loads default behaviour
-          ["core.concealer"] = {}, -- Adds pretty icons to your documents
-          ["core.keybinds"] = {}, -- Adds default keybindings
-          ["core.completion"] = {
-            config = {
-              engine = "nvim-cmp",
-            },
-          }, -- Enables support for completion plugins
-          ["core.journal"] = {}, -- Enables support for the journal module
-          ["core.dirman"] = { -- Manages Neorg workspaces
-            config = {
-              workspaces = {
-                notes = "~/notes",
-              },
-              default_workspace = "notes",
-            },
-          },
-        },
-      },
+    -- {
+       -- "nvim-neorg/neorg",
+       -- build = ":Neorg sync-parsers",
+       -- ft = "norg",
+       -- LocalLeader = ";",
+       -- dependencies = { "nvim-lua/plenary.nvim" },
+       -- vent = "VeryLazy",
+       -- opts = {
+         -- load = {
+           -- ["core.defaults"] = {
+           -- }, -- Loads default behaviour
+           -- ["core.concealer"] = {}, -- Adds pretty icons to your documents
+           -- ["core.keybinds"] = {}, -- Adds default keybindings
+           -- ["core.completion"] = {
+            -- config = {
+               -- engine = "nvim-cmp",
+             -- },
+           -- }, -- Enables support for completion plugins
+           -- ["core.journal"] = {}, -- Enables support for the journal module
+           -- ["core.dirman"] = { -- Manages Neorg workspaces
+             -- config = {
+               -- workspaces = {
+                 -- notes = "~/notes",
+               -- },
+               -- default_workspace = "notes",
+             -- },
+           -- },
+         -- },
+       -- },
+   -- },
+  {
+    "nvim-neorg/neorg",
+    lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+    version = "*", -- Pin Neorg to the latest stable release
+    config = true,
   },
   {
     "rafamadriz/friendly-snippets",
